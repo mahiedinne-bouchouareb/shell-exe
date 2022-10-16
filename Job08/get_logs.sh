5c1d580b58c314140df17271b1c9f2a352a3554a
mahiedinne-bouchouareb <91871267+mahiedinne-bouchouareb@users.noreply.github.com>
2022-10-16 18:03:23 +0200

    Update get_logs.sh

--- Job08/get_logs.sh
+++ Job08/get_logs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-d=nombre_ connexions_$(date +%d-%m-%Y-'%H:%M')
+d=nombre_connexions_$(date +%d-%m-%Y-'%H:%M')
 cat /var/log/auth.log | grep $USER | wc -l > $d
 tar -cvf ./$d.tar ./ && mv $d.tar Backup
