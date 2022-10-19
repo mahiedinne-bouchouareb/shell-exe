316da29a371ba3f0a23ec7d061b56e557dc61367
mahiedinne-bouchouareb <91871267+mahiedinne-bouchouareb@users.noreply.github.com>
2022-10-19 11:44:21 +0200

    modification du fichier

--- Job09/accessrights.sh
+++ Job09/accessrights.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+Input="/home/mahiedinne/Documents/Script/Job09/Shell_Userlist.csv"
+
+{
+read
+while IFS=, read -r Id Prenom Nom Mdp Role || [ -n "$Role" ];
+ do
+    echo  "  Id Prenom Nom Mdp Role"
+    paste -d "-"    
+    user=$Prenom$Nom
+    usernew="$(echo "$user" | tr -d "[:space:]")"
+    sudo useradd -p $(openssl passwd -1 $Mdp) $usernew
+    if [[ $Role == A* ]]; then
+        sudo usermod -aG sudo $usernew
+    fi
+
+done
+} < $Input
